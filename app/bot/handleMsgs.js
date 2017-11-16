775796f03b11dd8fd80c13573eb1892f1b63c68e
Chandrakanth <praneeth7516@gmail.com>
2017-11-16 11:51:36 -0800

    Added inital payload

--- app/bot/handleMsgs.js
+++ app/bot/handleMsgs.js
@@ -384,7 +384,7 @@ var goToCart = function(list){
                 "currency":"USD",
                 "payment_method":"Visa 2345",
                 "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
-                "timestamp":dt,
+                "timestamp":1428444852,
                 "address":{
                     "street_1":"1 Hacker Way",
                     "street_2":"",
