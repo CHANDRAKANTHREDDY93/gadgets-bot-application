9896f7a97307d2a7cd544657116322f5edc9ee85
Chandrakanth <praneeth7516@gmail.com>
2017-11-15 19:42:22 -0800

    Testing bot

--- public/images/gad_bot - Messenger - Facebook for Developers_files/IZeZz_6mhlQ.js
+++
@@ -1,3 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["krWbU"]); }
-
-__d("DeveloperDialogValidation",[],(function a(b,c,d,e,f,g){var h={init:function i(j){this._dialog=j;j.subscribe("success",this._handleSuccess.bind(this))},_handleSuccess:function i(j,k){var l=k.getData(),m=l.response.payload;if(m!==null&&m.success)this._dialog.hide()}};f.exports=h}),null);
