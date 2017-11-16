099d7046123f91a9565a628b931bc3c5d42ff2e6
Chandrakanth <praneeth7516@gmail.com>
2017-11-16 13:23:17 -0800

    Added inital payload

--- app/bot/handleMsgs.js
+++ app/bot/handleMsgs.js
@@ -399,7 +399,7 @@ var goToCart = function(list){
                     "subtotal":subTotal,
                     "shipping_cost":shipping_cost,
                     "total_tax": 23.33,
-                    "total_cost": total_cost
+                    "total_cost": 234.44
                 },
                 "adjustments":[
                     {
